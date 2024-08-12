364ac63acc1ab1de1e179d93572eb64398fb744e
AnushaDonthula <162703733+AnushaDonthula@users.noreply.github.com>
2024-08-12 20:31:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,17 +2,14 @@
 const draggables = document.querySelectorAll('.draggable');
 const container = document.getElementById('container');
 
-let draggedElement = null;
-
 draggables.forEach(draggable => {
     draggable.addEventListener('dragstart', (e) => {
-        draggedElement = e.target;
         e.target.classList.add('dragging');
+        e.dataTransfer.setData('backgroundImage', e.target.style.backgroundImage);
     });
 
     draggable.addEventListener('dragend', (e) => {
         e.target.classList.remove('dragging');
-        draggedElement = null;
     });
 });
 
@@ -23,20 +20,9 @@ container.addEventListener('dragover', (e) => {
     if (afterElement == null) {
         container.appendChild(draggable);
     } else {
+		const afterElementBg = afterElement.style.backgroundImage;
+        afterElement.style.backgroundImage = e.dataTransfer.getData('backgroundImage');
+        draggable.style.backgroundImage = afterElementBg;
         container.insertBefore(draggable, afterElement);
     }
 });
-
-function getDragAfterElement(container, y) {
-    const draggableElements = [...container.querySelectorAll('.draggable:not(.dragging)')];
-
-    return draggableElements.reduce((closest, child) => {
-        const box = child.getBoundingClientRect();
-        const offset = y - box.top - box.height / 2;
-        if (offset < 0 && offset > closest.offset) {
-            return { offset: offset, element: child };
-        } else {
-            return closest;
-        }
-    }, { offset: Number.NEGATIVE_INFINITY }).element;
-}
